0f1fc4fb31b80a647cdf1fddc6d932d63bdb66c3
Ilya Kokonov <chishok32@gmail.com>
2022-10-09 14:15:11 +0400

    Add in git

---
+++ Square.Tests/Square.Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Square.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Triangle_5_23_51_false()
+        {
+            double a = 5;
+            double b = 23;
+            double c = 51;
+
+            double actualSquare = Square.Triangle(a, b, c);
+
+            Assert.IsNaN(actualSquare);
+        }
+
+        [Test]
+        public void Triangle_3_4_5_6_and_rectangular()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+
+            double expectedSquare = 6;
+
+            double actualSquare = Square.Triangle(a, b, c);
+
+            bool flag = actualSquare == expectedSquare;
+
+            Assert.True(flag);
+        }
+
+        [Test]
+        public void Triangle_2_2_3_1_98431()
+        {
+            double a = 2;
+            double b = 2;
+            double c = 3;
+
+            double expectedSquare = 2;
+            double actualSquare = Square.Triangle(a, b, c);
+           
+            
+            bool flag = expectedSquare == Math.Round(actualSquare);
+            
+            Assert.True(flag);
+        }
+        [Test]
+        public void Circle_2_13()
+        {
+            double r = 2;
+
+            double expectedSquare = 13;
+            double actualSquare = Square.Circle(r);
+            
+            Assert.AreEqual(expectedSquare, Math.Round(actualSquare));
+        }
+        [Test]
+        public void Circle_9_254()
+        {
+            double r = 9;
+
+            double expectedSquare = 254;
+            double actualSquare = Square.Circle(r);
+            
+            Assert.AreEqual(expectedSquare, Math.Round(actualSquare));
+        }
+    }
+}
---
+++ Square/Square.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Square
+{
+    public static class Square
+    {
+        public static double Triangle(double a, double b, double c)
+        {
+            double P = 1;
+            double S = 0;
+            
+            if (a == 0 || b == 0 || c == 0)
+            {
+                return 0;
+            }
+
+            if (a > b && a > c)
+            {
+                if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+                {
+                    Console.Write("Треугольник прямоугольный \n");
+                }
+            } else if (b > a && b > c)
+            {
+                if (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))
+                {
+                    Console.Write("Треугольник прямоугольный \n");
+                }
+            } else if (c > a && c > b)
+            {
+                if (Math.Pow(c, 2) == Math.Pow(b, 2) + Math.Pow(a, 2))
+                {
+                    Console.Write("Треугольник прямоугольный \n");
+                }
+            }
+
+            if (a + b > c || a + c > b || c + b > a)
+            {
+                P = (a + b + c) / 2;
+                S = Math.Pow((P * (P - a) * (P - b) * (P - c)), 0.5);
+            }
+            else
+            {
+                return 0;
+            }
+
+            return S;
+        }
+
+        public static double Circle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+    }
+}
